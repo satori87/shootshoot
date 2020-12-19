d315ee3378767aa45445c2b33bee9502cbacf003
MacAroniCap <codingzombie@gmail.com>
2020-12-19 17:12:35 -0500

    refactored the code some (mostly PlayScene), also set Hank as default tank

--- src/main/java/com/bg/badgame/core/Prop.java
+++ src/main/java/com/bg/badgame/core/Prop.java
@@ -3,16 +3,18 @@ package com.bg.badgame.core;
 import com.bg.bearplane.engine.BearTool;
 
 public class Prop {
-	public float x, y, rot, rad;
+	public Position position = new Position();
+	public float rotation;
+	public float radius;
 	public int id, type;
 	public boolean npc = false;
 
 	public Prop(int id) {
 		this.id = id;
 		type = BearTool.rndInt(0, 6);
-		x = (float) ((Math.random() * (BadGame.GAME_WIDTH-40) * 2) / 2) + 40;
-		y = (float) ((Math.random() * (BadGame.GAME_HEIGHT-40) * 2) / 2) + 40;
-		rot = BearTool.randInt(0, 359);
-		rad = 16;
+		position.x = (float) ((Math.random() * (BadGame.GAME_WIDTH-40) * 2) / 2) + 40;
+		position.y = (float) ((Math.random() * (BadGame.GAME_HEIGHT-40) * 2) / 2) + 40;
+		rotation = BearTool.randInt(0, 359);
+		radius = 16;
 	}
 }
